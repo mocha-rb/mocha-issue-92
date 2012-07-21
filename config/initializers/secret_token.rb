703fc52c23cab08919ef8b6dc330ee6abd7432c7
James Mead <james@floehopper.org>
2012-07-21 17:35:24 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MochaIssue92::Application.config.secret_token = 'f27e480a298e6586e1226eca639744e26c792c981014386fa4e067b4940f64a28dc43274b85785b597f3f9a37d04c7c47da3f9aeb06ae7bbba9381685f793707'
